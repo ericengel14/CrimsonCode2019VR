905640e870c81fe8645894ea228e5f3d63c9188d
Cpts224-2018 <gitcpts224@gmail.com>
2019-02-23 14:28:01 -0800

    Finished backend.

---
+++ gvr-unity-sdk-master/Assets/PlayerScore.cs
@@ -0,0 +1,43 @@
+﻿using Proyecto26;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour
+{
+    public Text scoreText;
+    public InputField nameText;
+
+    private System.Random random = new System.Random();
+
+    public static int playerScore;
+    public static string playerName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScore = random.Next(0, 100);
+        scoreText.text = "Score: " + playerScore;
+    }
+
+    public void OnSubmit()
+    {
+        playerName = nameText.text;
+        postToDatabase();
+    }
+
+    private void postToDatabase()
+    {
+        User user = new User();
+        RestClient.Post( url: "https://daydream-b0bf1.firebaseio.com/.json", user);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ gvr-unity-sdk-master/Assets/User.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User 
+{
+    public string userName;
+    public int userScore;
+
+    public User()
+    {
+        userName = PlayerScore.playerName;
+        userScore = PlayerScore.playerScore;
+    }
+    
+}
