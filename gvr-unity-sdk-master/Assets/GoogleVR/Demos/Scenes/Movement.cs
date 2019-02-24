cda98d8fbeb929f6137f84ba9648395414ee6658
Eric Engel <eric.engel@wsu.edu>
2019-02-24 03:20:28 -0800

    Fixed hopping.

--- gvr-unity-sdk-master/Assets/GoogleVR/Demos/Scenes/Movement.cs
+++ gvr-unity-sdk-master/Assets/GoogleVR/Demos/Scenes/Movement.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
-    public float distance = 6;
+    public float distance = 5;
     public bool movement = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 temp = transform.position;
+        temp.y = 1;
+        transform.position = temp;
     }
 
     // Update is called once per frame
@@ -19,17 +21,26 @@ public class Movement : MonoBehaviour
     {
         if (GvrControllerInput.ClickButtonDown)
         {
+            Vector3 temp = transform.position;
+            temp.y = 1;
+            transform.position = temp;
             movement = true;
         }
 
         if(GvrControllerInput.ClickButtonUp)
         {
+            Vector3 temp = transform.position;
+            temp.y = 1;
+            transform.position = temp;
             movement = false;
         }
 
         if (movement)
         {
             transform.position = transform.position + Camera.main.transform.forward * distance * Time.deltaTime;
+            Vector3 temp = transform.position;
+            temp.y = 1;
+            transform.position = temp;
         }
     }
 }
